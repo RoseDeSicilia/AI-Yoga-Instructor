90abf18ba3007e72a2602d99df80fae26b3f19c2
Rose De Sicilia <90062441+RoseDeSicilia@users.noreply.github.com>
2022-02-22 13:53:19 -0500

    Update detector.py

--- workingDetector/detector.py
+++ workingDetector/detector.py
@@ -4,7 +4,7 @@ import cv2
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 
-cap = cv2.VideoCapture('bodyweight-male-squat-front.mp4')
+cap = cv2.VideoCapture(0)
 # Initiate holistic model
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     
